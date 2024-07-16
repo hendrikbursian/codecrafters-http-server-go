db50a28f840a23cb87ff79cafb6d71cdaf515c3c
Hendrik Bursian <hendrikbursian@protonmail.com>
2024-07-16 12:29:45 +0200

    Add ok response

--- app/server.go
+++ app/server.go
@@ -16,9 +16,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	res := []byte("HTTP/1.1 200 OK\r\n\r\n")
+	conn.Write(res)
 }
