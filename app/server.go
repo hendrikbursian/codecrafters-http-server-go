079ea246d9748effced8ad6d52e56a063dcc0c00
Hendrik Bursian <hendrikbursian@protonmail.com>
2024-07-16 12:58:45 +0200

    Read path from request

--- app/server.go
+++ app/server.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
+	"strings"
+)
+
+const HTTP_VERSION = "HTTP/1.1"
+const (
+	HTTP_STATUS_NOT_FOUND = "404 Not Found"
+	HTTP_STATUS_OK        = "200 OK"
 )
 
 func main() {
@@ -21,7 +29,39 @@ func main() {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	defer conn.Close()
 
-	res := []byte("HTTP/1.1 200 OK\r\n\r\n")
-	conn.Write(res)
+	var buf []byte = make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading request: ", err.Error())
+		os.Exit(1)
+	}
+	req := string(buf[:n])
+	fmt.Println("Request: ", req)
+
+	reqSplit := strings.Split(req, "\r\n")
+
+	reqLine := strings.Split(reqSplit[0], " ")
+	// verb := reqLine[0]
+	path := reqLine[1]
+	// version := reqLine[2]
+
+	var res bytes.Buffer
+
+	// response line
+	res.WriteString(HTTP_VERSION + " ")
+	if path != "/" {
+		res.WriteString(HTTP_STATUS_NOT_FOUND)
+	} else {
+		res.WriteString(HTTP_STATUS_OK)
+	}
+	res.WriteString(" ")
+	res.WriteString("\r\n\r\n")
+
+	n, err = conn.Write(res.Bytes())
+	if err != nil {
+		fmt.Println("Error sending response: ", err)
+		os.Exit(1)
+	}
 }
